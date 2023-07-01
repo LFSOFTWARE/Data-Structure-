5476584411d44026a6a04ab9118a1632d5478992
Luiz <luiz.ssantos.ext@infracommerce.com.br>
2023-06-30 22:58:24 -0300

    feat: add method

--- src/linked-list/linked-list.ts
+++ src/linked-list/linked-list.ts
@@ -1,16 +1,31 @@
-import  NodeElement  from './node-list';
+import NodeElement from './node-list';
 
 export default class LinkedList {
-  head: any;
-  tail: any;
-  size: number;
+  head: any
+  tail: any
+  size: number
 
   constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
+    this.head = null
+    this.tail = null
+    this.size = 0
   }
+
   add(element: any): void {
     const node = new NodeElement(element);
+    let current
+
+    if (this.head === null) {
+      this.head = node
+    } else {
+      current = this.head
+      
+      while (current.next) {
+        current = current.next
+      }
+
+      current.next = node
+      this.size += 1
+    }
   }
 }
